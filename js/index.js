0e6a85d23dfc5d3d0a4eee483720a366f658d492
Dragos Circa <circadragos@yahoo.com>
2017-07-17 11:58:04 +0300

    Finalize index.js
    Modificari:
    -index.html: adaugat taguri 'data-...'
    schimbat nume de clase
    -index.css: adaugat css pentru functionalitate
    -index.js: inchieiat script pt index: filtru (search + butoane), lista dreapta, comanda.

--- js/index.js
+++ js/index.js
@@ -3,6 +3,14 @@ $(document).ready(function()
     function UpdatePrice(price)
     {
         $('#total-price').html(parseFloat($('#total-price').html()) + price);
+        $('#modalComanda .price').html(parseFloat($('#modalComanda .price').html()) + price);
+    }
+
+    function ClearFilter()
+    {
+        $('#filter-bar button').removeClass("btn-active");
+
+        $('#products > .row div.hidden').removeClass("hidden");
     }
 
 
@@ -34,7 +42,7 @@ $(document).ready(function()
 
     $('.product-thumbnail').on('click',function()
     {
-        var template = $('#templates').children('#product');
+        var template = $('#templates').children('.product');
         var price = $(this).attr('data-price');
 
         $(template).children(".bascket-product").children('.name').html(
@@ -47,5 +55,52 @@ $(document).ready(function()
 
         $('#produse-in-cos').append($(template).html());
         UpdatePrice(parseFloat(price));
+    });
+
+    $('.btn-filter').on('click', function(){
+        if ($(this).hasClass('btn-active'))
+        {
+            ClearFilter();
+            return;
+        }
+        ClearFilter();
+        var $currentButton = $(this);
+
+        $(this).addClass('btn-active');
+        $('#products > .row:first-child > div').each(function(index,value){
+            if ($(this).attr('data-category') != $currentButton.attr('data-category'))
+                $(this).addClass('hidden');
+        });
+    });
+
+    $('#filter-search').on('keyup',function(){
+        if ($(this).val() == '') 
+        {
+            $('#products > .row div.hidden').removeClass("hidden");
+            return;
+        }
+        var $inputRef = $(this);
+
+        $('#products > .row:first-child > div').each(function(index,value){
+            if (!$(this).find('h4').text().toLowerCase().match("^" + $inputRef.val().toLowerCase()))
+                $(this).addClass('hidden');
+            else
+                $(this).removeClass('hidden');
+        });
+    });
+
+    $('#confirm-basket').on('click',function(){
+        var currentCredit = parseFloat($('#total-credit').text());
+        var totalPrice = parseFloat($('#total-price').text());
+    })
+    $('#basket-trigger-btn').on('click',function(){
+        var $target = $('#modalComanda .row > div.col-md-5 > ol');
+        $('#produse-in-cos .btn-group').each(function(index, value){
+            var $name = $(this).find('.name');
+            var $quantity = $(this).find('.quantity');
+
+            $target.append('<li>' + $name.text() + ' x' + $quantity.text() + '</li>');
+            //$target.append('<li>' +  i + ': ' + $name + ' x' + $quantity + '</li>');
+        });
     });
 });
