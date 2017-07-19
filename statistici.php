bb2f15126bebd697b0fba7c6cd1e72594fe3e98c
StefanCiuclea <victor.ciuclea@ulbsibiu.ro>
2017-07-19 10:54:24 +0300

    Log.php Statistici.php Istoric.php
    Adaugat functionalitate la datetimepicker

--- statistici.php
+++ statistici.php
@@ -6,6 +6,10 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 	integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	
+	<!--Datepicker CSS-->
+	
+	<link rel="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css"/>
+	
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
 	integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
@@ -103,5 +107,19 @@
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 	integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	
+	<!--Moments JS -->
+	<script src="https://momentjs.com/downloads/moment.min.js"></script>
+	<!--Datepicker JS-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+	<script type="text/javascript">
+            $(function () {
+				moment.locale('en',{
+					week: {dow: 1}
+				});
+                $('#fromDate').datetimepicker();
+				$('#toDate').datetimepicker();
+            });
+    </script>
 </body>
 </html>
